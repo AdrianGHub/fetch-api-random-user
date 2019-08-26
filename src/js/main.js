81c37bf72418a6f1972a3989c74d6166a22d90a3
Adrian <adrian_zinko@op.pl>
2019-08-26 15:37:04 +0200

    feat: create get function for fetching users

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
 "use strict";
 
+const getRandomUsers = () => {
+  const url = "https://randomuser.me/api/?results=10";
+
+  fetch(url)
+    .then(res => {
+      if (res.status !== 200) {
+        throw Error("This isn't 200 status response");
+      } else {
+        return res.json();
+      }
+    })
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+};
+
+document.querySelector("button").addEventListener("click", getRandomUsers);
+
 console.log(`Hello world!`);
-
-
